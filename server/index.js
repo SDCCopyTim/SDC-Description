01c551963d11d5f7aa2cb7f31d63d93001afb153
Tracy <ymailingtracy@yahoo.com>
2020-09-28 01:10:45 -0700

    Finally got my html elements rendering

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ const router = require('./router.js');
 const app = express();
 const PORT = 3002;
 
-app.use('/', express.static(path.join(__dirname + '../client/public')));
+app.use('/', express.static(path.join(__dirname + '/../client/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/', router);
 
 
 
