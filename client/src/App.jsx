a92c1e17bb2ca5cd5433b0c3ea27683894814f4d
Tracy <ymailingtracy@yahoo.com>
2020-09-28 01:44:29 -0700

    Added html / css to style the outline of the modules.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import '../public/style.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,26 +14,28 @@ class App extends React.Component {
   render() {
 
     return (
-    <div id="MainContainer">
-      <div class="TitleModule">
-        <h1>Title Module</h1>
+      <div id="MainContainer">
+      <div class="LeftModule">
+         <div class="TitleModule">
+           <h1>Title Module</h1>
+         </div>
+         <div class="DescriptionModule">
+           <h1>Description Module</h1>
+         </div>
+         <div class="AmenitiesEssentialsLodging">
+           <h1>Amentities Essentials Lodging</h1>
+         </div>
+         <div class="QuestionsModule">
+           <h1>Questions</h1>
+         </div>
+         <div class="Details">
+           <h1>Details</h1>
+         </div>
       </div>
-      <div class="DescriptionModule">
-        <h1>Description Module</h1>
-      </div>
-      <div class="AmenitiesEssentialsLodging">
-        <h1>Amentities Essentials Lodging</h1>
-      </div>
-      <div class="QuestionsModule">
-        <h1>Questions</h1>
-      </div>
-      <div class="Details">
-        <h1>Details</h1>
-      </div>
-      <div class="Calendar">
-        <h1>Calendar</h1>
-      </div>
-    </div>
+         <div class="Calendar">
+           <h1>Calendar</h1>
+         </div>
+       </div>
     );
   };
 };
