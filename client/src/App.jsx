b251e8b172ff8ba6b4ba3ae5a68b1f4045109aa1
Tracy <ymailingtracy@yahoo.com>
2020-10-05 11:40:05 -0700

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import { BsChevronBarDown } from "react-icons/bs";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -33,7 +34,7 @@ class App extends React.Component {
       checkin: false,
       checkout: false,
       checkIndate: new Date(),
-      value: 0
+      value: 1
     };
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
@@ -51,7 +52,7 @@ class App extends React.Component {
   };
   componentDidMount() {
     // axios.get(`/one${this.state.id}`)
-    axios.get('/all')
+    axios.get(`http://localhost:3002/one${this.state.id}`)
       .then((results) => {
         this.setState({
           campsites: results.data,
@@ -171,12 +172,12 @@ class App extends React.Component {
     const { selectedDay, isDisabled, isEmpty } = this.state;
     const checkin = this.state.checkin;
     const checkout = this.state.checkout;
-    const price = this.state.currCampSite.costs;
+    var price = this.state.currCampSite.costs;
 
     if(this.state.value > 0){
       price = price * this.state.value;
-      return price;
     }
+
 
     return (
       <div>
@@ -282,8 +283,7 @@ class App extends React.Component {
               <div className="Calendar">
                 <div className="newCalendar">
 
-                  <Calendar onChange={this.onChange}
-                    value={this.state.date} />
+                  <Calendar onChange={this.onChange} value={this.state.date} />
                 </div>
               </div>
             </div>
@@ -327,7 +327,7 @@ class App extends React.Component {
                     <div className="Recommends">
 
                       <div className="Recommend">
-                        <a><FaThumbsUp />{this.state.currCampSite.recommended} </a>
+                        <a className="thumbs"><FaThumbsUp />{this.state.currCampSite.recommended} </a>
                         <div className="color">Recommend
                 </div>
                       </div>
@@ -398,7 +398,7 @@ class App extends React.Component {
                 <div className="Calendar">
                   <div className="smolCalendar">
                     <div className="Cost">
-                      <b>{price}</b>
+                      <b>${price}</b>
                       <div className="pernight">per night</div>
                     </div>
                     <div className="CheckInCheckOutGuests">
@@ -429,126 +429,3 @@ class App extends React.Component {
 
 
   export default App;
-
-
-
-
-
-      // return (
-      //   <div>
-      //     <Modal id="modal" show={this.state.show} handleClose={this.hideModal} onClick={this.showModal}  />
-
-      //     <AmentitiesModal id="modal" show={this.state.showAmentities} handleClose={this.hideAmentities} onClick={this.showAmentitiesModal} amentities={this.state.amentities}/>
-
-      //     <LodgingModal id="modal" show={this.state.showLodging} handleClose={this.hideLodging} onClick={this.showLodgingModal} lodging={this.state.lodging}/>
-
-      //     <EssentialsModal id="modal" show={this.state.showEssentials} handleClose={this.hideEssentials} onClick={this.showEssentialsModal} essentials={this.state.essentials}/>
-
-      //     <div className="MainContainer">
-      //       <div className="LeftModule">
-      //         <div className="TitleModule">
-      //           <ul className="nav">
-      //             <li><a>United States</a></li>
-      //             <li><a> > </a></li>
-      //             <li><a href="#States">{this.state.currCampSite.States}</a></li>
-      //             <li><a> > </a></li>
-      //             <li><a href="#Farms">{this.state.currCampSite.Farms}</a></li>
-      //             <li><a> > </a></li>
-      //           </ul>
-
-      //           <div className="CampSite">
-      //             <h1>{this.state.currCampSite.Farms}</h1>
-      //             <p><b>Nearby:</b> {this.state.currCampSite.Parks}</p>
-      //           </div>
-
-      //           <div className="Recommends">
-
-      //             <div className="Recommend">
-      //               <a><FaThumbsUp />{this.state.currCampSite.recommended} </a>
-      //               <div className="color">Recommend
-      //             </div>
-      //             </div>
-
-      //             <div className="Photos">
-      //               <img src={this.state.photos[0]} className="circle" id="circle1" />
-      //               <img src={this.state.photos[1]} className="circle" id="circle2" />
-      //               <img src={this.state.photos[2]} className="circle" id="circle3" />
-      //               <img src={this.state.photos[3]} className="circle" id="circle4" />
-      //               <div className="circle" id="notCircle">{this.state.currCampSite.responses}</div>
-      //             </div>
-      //             <div className="Share">
-      //               <div className="A" onClick={this.showModal}><a className="a"><FaCamera /><div className="upload">Upload</div></a>
-      //               </div>
-      //               <div className='B' onClick={this.showModal}><a className='b'><BsChevronBarDown /><div className="save">Save To List</div></a>
-      //               </div>
-      //               <div className='C'><a className='c'><MdShare /></a>
-      //               </div>
-      //             </div>
-      //           </div>
-
-      //         </div>
-      //         <br></br>
-      //         <div className="DescriptionModule">
-      //           <p>
-      //           </p>{this.state.currCampSite.description}
-
-      //         </div>
-
-
-      //         <br></br>
-
-      //         <div className="AmentitiesEssentialsLodging">
-      //           <div className="Lodging"><b>Lodging</b>
-      //             <Lodging lodging={this.state.lodging} showLodgingModal={this.showLodgingModal}/>
-      //           </div>
-      //           <div className="Essentials"><b>Essentials</b>
-      //             <Essentials essentials={this.state.essentials} showEssentialsModal={this.showEssentialsModal}/>
-      //           </div>
-      //           <div className="Amentities"><b>Amentities</b>
-      //             <Amentities amentities={this.state.amentities} showAmentitiesModal={this.showAmentitiesModal}/>
-      //           </div>
-      //         </div>
-
-      //         <div className="QuestionsModule" onClick={this.showModal}>
-      //           Have a Question? <div className="owners">Send {this.state.currCampSite.Owners} a message! </div>
-      //         </div>
-      //         <div className="Details">
-      //           <div className="details">Details
-      //        </div>
-      //           <div className="checkIn">
-      //             <ul>
-      //               <li>Check In: {this.state.currCampSite.checkIn}</li>
-      //               <li>Check Out: {this.state.currCampSite.checkOut}</li>
-      //               <li>Cancellation Policy: {this.state.currCampSite.cancellation}</li>
-      //             </ul>
-      //           </div>
-      //           <div className="onArrival">
-      //             <ul>
-      //               <li>On Arrival: {this.state.currCampSite.onArrival}</li>
-      //               <li>Minimum Nights: {this.state.currCampSite.minimumNights}</li>
-      //               <li>Accepts Bookings: {this.state.currCampSite.acceptsBookings}</li>
-      //             </ul>
-      //           </div>
-
-      //         </div>
-      //       </div>
-      //       <div className="Calendar">
-      //         <div className="smolCalendar">
-      //           <div className="Cost">
-      //             <b>${this.state.currCampSite.costs}</b>
-      //             <div className="pernight">per night</div>
-      //           </div>
-      //           <div className="CheckInCheckOutGuests">
-      //             <div className="checkin"><b>Check In</b><br></br>
-      //           Select Date</div>
-      //             <div className="checkout"><b>Check Out</b><br></br>
-      //           Select Date</div>
-      //             <div className="guests"><b>Guests</b><br></br>
-      //           Select Date</div>
-      //           </div>
-      //           <div className="InstantBook">Instant Book</div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // );
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const router = require('./router.js');
 const app = express();
 const PORT = 3002;
 
-app.use('/',express.static(path.join(__dirname + '/../client/public')));
+app.use('/:id',express.static(path.join(__dirname + '/../client/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', router);
